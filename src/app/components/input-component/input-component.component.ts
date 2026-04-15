168661e64b46e066936d5e223e73136754187b92
evelia <eveliagil@gmail.com>
2026-04-15 16:31:11 +0200

    creacion de los components

---
+++ src/app/components/input-component/input-component.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input-component',
+  imports: [],
+  templateUrl: './input-component.component.html',
+  styleUrl: './input-component.component.scss'
+})
+export class InputComponentComponent {
+
+}
