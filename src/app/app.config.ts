1bdd32aa1d00b58933307f3d4d1e0600dc158efa
evelia <eveliagil@gmail.com>
2026-04-15 16:34:15 +0200

    creacion de la internacionalizacion

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,23 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 
+import { provideTranslateService } from '@ngx-translate/core';
+import { provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([])),
+    provideTranslateService({
+      defaultLanguage: localStorage.getItem('idioma_seleccionado') ?? 'es',
+      fallbackLang: localStorage.getItem('idioma_seleccionado') ?? 'es',
+      loader: provideTranslateHttpLoader({
+        prefix: './assets/i18n/',
+        suffix: '.json',
+      }),
+    }),
+  ],
 };
