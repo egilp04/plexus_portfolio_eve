4968015612d863196f0a06922dd3ef05af60a850
evelia <eveliagil@gmail.com>
2026-04-15 16:50:38 +0200

    creacion de la pipe personalizada años de experiencia

---
+++ src/app/pipes/anio-experiencia.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { AnioExperienciaPipe } from './anio-experiencia.pipe';
+
+describe('AnioExperienciaPipe', () => {
+  it('create an instance', () => {
+    const pipe = new AnioExperienciaPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/pipes/anio-experiencia.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+@Pipe({
+  name: 'anioExperiencia',
+})
+export class AnioExperienciaPipe implements PipeTransform {
+  transform(value: number | string): string {
+    if (!value) return '';
+
+    const anioInicio = Number(value);
+    const anioActual = new Date().getFullYear();
+    const experiencia = anioActual - anioInicio;
+
+    return `${experiencia} años de experiencia`;
+  }
+}
