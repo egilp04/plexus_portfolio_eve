55974c885fb4e68a6a8a5d01a1398d5c23c658e4
evelia <eveliagil@gmail.com>
2026-04-16 16:49:43 +0200

    pagina de contacto

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
-import { InputComponentComponent } from "../../components/input-component/input-component.component";
+import { TitleCasePipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact',
-  imports: [InputComponentComponent],
+  imports: [ReactiveFormsModule, TranslateModule, TitleCasePipe],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss',
   standalone: true,
 })
-export class ContactComponent {}
+export class ContactComponent {
+  private formBuilder = inject(FormBuilder);
+  profileForm = this.formBuilder.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    address: ['', [Validators.required, Validators.minLength(4)]],
+  });
+
+  onSubmit() {
+    if (this.profileForm.invalid) {
+      alert('hay campos invalidos');
+      return;
+    }
+    alert('Enviado!!!');
+  }
+}
