4eb3f5ee18a74fa29fe2efe5dc8a3518683aae6d
evelia <eveliagil@gmail.com>
2026-04-16 12:18:50 +0200

    landing finalizada

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -1,11 +1,14 @@
 import { UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
-import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { NavbarComponent } from '../../components/navbar/navbar.component';
+
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-landing',
-  imports: [UpperCasePipe, TranslateModule, NavbarComponent],
+  imports: [UpperCasePipe, TranslateModule, NavbarComponent, MatIconModule],
+
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss',
   standalone: true,
