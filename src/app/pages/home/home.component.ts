779247b41ee0fd75e6d4dceb73b9d3dbd1a9b6b3
evelia <eveliagil@gmail.com>
2026-04-20 09:27:54 +0200

    cambios en el about html y en el home de los usuarios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,36 @@
-import { Component, inject } from '@angular/core';
 import { SessionService } from '../../services/session.service';
 import { TranslateModule } from '@ngx-translate/core';
+import {
+  Component,
+  AfterViewInit,
+  Inject,
+  PLATFORM_ID,
+  inject,
+} from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
+import { gsap } from 'gsap';
 @Component({
   selector: 'app-home',
-  imports: [TranslateModule],
+  imports: [TranslateModule, MatIconModule, MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   standalone: true,
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
   userSession = inject(SessionService);
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  ngAfterViewInit(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      gsap.from('.hero-section', {
+        opacity: 0,
+        y: 40,
+        duration: 1,
+        ease: 'power3.out',
+      });
+    }
+  }
 }
