ffd7b099d0246486ec617025396930a7134eaa30
evelia <eveliagil@gmail.com>
2026-04-20 10:22:48 +0200

    efecto match media

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,7 @@ import {
   Inject,
   PLATFORM_ID,
   inject,
+  OnDestroy,
 } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
@@ -19,18 +20,42 @@ import { gsap } from 'gsap';
   styleUrl: './home.component.scss',
   standalone: true,
 })
-export class HomeComponent implements AfterViewInit {
+export class HomeComponent implements AfterViewInit, OnDestroy {
   userSession = inject(SessionService);
+  private mm!: gsap.MatchMedia;
 
   constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
   ngAfterViewInit(): void {
     if (isPlatformBrowser(this.platformId)) {
-      gsap.from('.hero-section', {
-        opacity: 0,
-        y: 40,
-        duration: 1,
-        ease: 'power3.out',
+      this.mm = gsap.matchMedia();
+      this.mm.add('(max-width: 767px)', () => {
+        const tlMobile = gsap.timeline();
+        tlMobile.from('.hero-section', {
+          opacity: 0,
+          y: 30,
+          duration: 0.5,
+          stagger: 0.1,
+        });
       });
+
+      this.mm.add('(min-width: 768px)', () => {
+        const tlDesktop = gsap.timeline();
+        tlDesktop.from('.hero-section', {
+          opacity: 0,
+          x: -100,
+          rotation: -5,
+          duration: 1,
+          stagger: 0.2,
+          ease: 'back.out(1.5)',
+        });
+      });
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this.mm) {
+      this.mm.revert();
     }
   }
 }
