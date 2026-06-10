35ff80c4d1b542f404aa8742cc6041d255627d20
egilp04 <egilp04@iesalbarregas.es>
2026-06-10 13:48:36 +0200

    .

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -21,35 +21,7 @@ import technolgies_data from '../../../assets/data/technolgies_data.json';
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent implements AfterViewInit {
-  @ViewChildren('timelineItem') items!: QueryList<ElementRef>;
+export class AboutComponent {
   timelineKeys = ['CORDOBA', 'MUNSTER', 'TRANSLATION', 'WEBDEV', 'PLEXUS'];
   techStack = technolgies_data;
-
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
-
-  ngAfterViewInit(): void {
-    if (isPlatformBrowser(this.platformId)) {
-      this.initEntranceAnimation();
-    }
-  }
-
-  private initEntranceAnimation() {
-    const htmlElements = this.items.map((item) => item.nativeElement);
-    const tl = gsap.timeline();
-    tl.to('.about-intro', {
-      opacity: 1,
-      y: 0,
-      duration: 0.4,
-      ease: 'power3.out',
-      delay: 0.5,
-    }).to(htmlElements, {
-      opacity: 1,
-      x: 0,
-      duration: 0.8,
-      ease: 'power3.out',
-      stagger: 0.3,
-      clearProps: 'all',
-    });
-  }
 }
